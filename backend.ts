ecc8459ccefa4bd2c56320d591ceabfad305536e
Mattool81974 <mmattmmo@gmail.com>
2022-12-02 18:32:19 +0100

    Test.7

--- backend.ts
+++ backend.ts
@@ -3,22 +3,12 @@ import { serve } from "https://deno.land/std@0.157.0/http/server.ts";
 const port = 80;
 
 const handler = async (request: Request): Promise<Response> => {
-  const resp = await fetch("https://api.github.com/users/denoland", {
-    // The init object here has an headers object containing a
-    // header that indicates what type of response we accept.
-    // We're not specifying the method field since by default
-    // fetch makes a GET request.
-    headers: {
-      accept: "application/json",
-    },
-  });
+  const resp = await fetch("page.html");
   console.log("Requête");
   console.log(resp.body);
   return new Response(resp.body, {
     status: resp.status,
-    headers: {
-      "content-type": "application/json",
-    },
+    
   });
 
 };
