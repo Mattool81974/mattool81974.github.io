ec6f455671f5e1e17a009ae6fe4df1ca9a29f566
Mattool81974 <mmattmmo@gmail.com>
2022-12-02 18:35:24 +0100

    Test.8

--- backend.ts
+++ backend.ts
@@ -3,7 +3,7 @@ import { serve } from "https://deno.land/std@0.157.0/http/server.ts";
 const port = 80;
 
 const handler = async (request: Request): Promise<Response> => {
-  const resp = await fetch("page.html");
+  const resp = Deno.open("page.html");
   console.log("Requête");
   console.log(resp.body);
   return new Response(resp.body, {
