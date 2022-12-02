e0fa028a060cea6379c0ce84fc536ef5497153ca
Mattool81974 <mmattmmo@gmail.com>
2022-12-02 18:55:56 +0100

    Test.14

--- backend.ts
+++ backend.ts
@@ -4,7 +4,9 @@ const port = 80;
 
 const handler = async (request: Request): Promise<Response> => {
   let texte = "";
-  const resp = Deno.open("page.txt").then(function(success) {texte=success;});
+  const resp = Deno.open("page.txt");
+  await copy(resp, Deno.stdout);
+  resp.then(function(success) {texte=success;});
   console.log("Requête");
   console.log(texte);
   return new Response(texte, {
